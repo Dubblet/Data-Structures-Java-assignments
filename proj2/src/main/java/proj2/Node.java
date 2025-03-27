75a53ace916a58b010f8e761821d7aaeeb997295
Steven Bruce <bruces92@gmail.com>
2025-03-27 18:36:45 -0400

    Created fields and constructors

--- proj2/src/main/java/proj2/Node.java
+++ proj2/src/main/java/proj2/Node.java
@@ -1,4 +1,39 @@
 package proj2;
 
 public class Node {
+    // Fields
+    private int exponent;
+    private int coefficient;
+    private Node nextNode;
+
+    public Node(int exponent, int coefficient) {
+        this.exponent = exponent;
+        this.coefficient = coefficient;
+        this.nextNode = null;
+    }
+
+    // Getters and Setters
+    public int getExponent() {
+        return exponent;
+    }
+
+    public void setExponent(int exponent) {
+        this.exponent = exponent;
+    }
+
+    public int getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
 }
--- proj2/src/main/java/proj2/Polynomial.java
+++ proj2/src/main/java/proj2/Polynomial.java
@@ -1,4 +1,36 @@
 package proj2;
 
 public class Polynomial {
+    // Fields
+    private Node monomial;
+
+    // Constructor
+    public Polynomial() {
+        monomial = null;
+    }
+
+    // Copy Constructor
+    public Polynomial(Polynomial otherPoly) {
+        Node otherPolyNode = otherPoly.monomial;
+        Node newMonomial = new Node(otherPolyNode.getExponent(), otherPolyNode.getCoefficient());
+        this.monomial = newMonomial;
+        Node currentNode = newMonomial;
+
+        while (otherPolyNode.getNextNode() != null) {
+            otherPolyNode = otherPolyNode.getNextNode();
+            Node newNode = new Node(otherPolyNode.getExponent(), otherPolyNode.getCoefficient());
+            currentNode.setNextNode(newNode);
+            currentNode = newNode;
+        }
+    }
+
+    // Getters and Setters
+    public Node getMonomial() {
+        return monomial;
+    }
+
+    public void setMonomial(Node monomial) {
+        this.monomial = monomial;
+    }
+
 }
