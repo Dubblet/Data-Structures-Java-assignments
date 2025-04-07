62625f7afb4c3982439863a60abb61940c137413
Steven Bruce <bruces92@gmail.com>
2025-04-07 19:39:10 -0400

    Added methods to linked list to add, remove, and modify nodes

--- proj3/src/main/java/proj3/ChangeStack.java
+++ proj3/src/main/java/proj3/ChangeStack.java
@@ -34,5 +34,64 @@ public class ChangeStack {
         this.top = top;
     }
 
+    public void push(int item) {
+        Node newNode = new Node(item);
+        newNode.setNext(top);
+        top = newNode;
+        numElements++;
+    }
 
+    public int pop() {
+        try {
+            int data = top.getData();
+            top = top.getNext();
+            numElements--;
+            return data;
+        } catch (NullPointerException e) {
+            throw new StackEmptyException("Stack is empty!", e);
+        }
+    }
+
+    public int peekTop() {
+        try {
+            return top.getData();
+        } catch (NullPointerException e) {
+            throw new StackEmptyException("Stack is empty!", e);
+        }
+    }
+
+    public void increaseValues(int k, int amount) {
+        Node cursor = getStackFromBottom(k);
+        while (cursor != null) {
+            cursor.setData(cursor.getData() + amount);
+            cursor = cursor.getNext();
+        }
+    }
+
+    public void decreaseValues(int k, int amount) {
+        Node cursor = getStackFromBottom(k);
+        while (cursor != null) {
+            cursor.setData(cursor.getData() - amount);
+            cursor = cursor.getNext();
+        }
+    }
+
+    private Node getStackFromBottom(int k) {
+        if (top == null) {
+            return null;
+        }
+        Node cursor = top;
+        int stackSize = numElements;
+        while (stackSize > k) {
+            cursor = cursor.getNext();
+            stackSize--;
+        }
+        return cursor;
+    }
+
+    private static class StackEmptyException extends RuntimeException {
+        public StackEmptyException(String errorMessage, Throwable error) {
+            super(errorMessage, error);
+        }
+    }
 }
